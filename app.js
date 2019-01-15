3e64037b7dfba7de72daf4d830f0f6e376fb61ba
Linda Allen <lindaallenx@gmail.com>
2019-01-15 19:57:38 +0000

    style show page
    * made new file main.css and put it inside of a public directory / stylesheets
      * serve public directory in app.js
      * include css link in header partial
    * style show page
      * add in new sidebar
      * style main content
      * add hardcoded price and comment date and link campground name
      * style width and padding of campground images
      * add padding to caption

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true 
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"))
 seedDB();
 
 // Campground.create(
