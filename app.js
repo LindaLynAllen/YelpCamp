9cee5d633f45c4b40f4df6fbedec703e24281b36
Linda Allen <lindaallenx@gmail.com>
2019-01-18 23:58:27 +0000

    login
    * add login routes
    * add login template

--- app.js
+++ app.js
@@ -160,6 +160,18 @@ app.post("/register", function(req, res){
     });
 });
 
+//show login form
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+//handling login logic
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds", 
+        failureRedirect: "/login"
+    }), function(req, res) {
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The YelpCamp Server Has Started");
 });
