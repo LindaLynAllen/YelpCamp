8b07da7351491fe2ef65cb6f675b3841d23f2b99
Linda Allen <lindaallenx@gmail.com>
2019-01-18 23:34:07 +0000

    registration
    * configure Passport
    * add register routes
    * add register template

--- app.js
+++ app.js
@@ -16,6 +16,18 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"))
 seedDB();
 
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret:"secret code",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 // Campground.create(
 //     {
 //         name: "Granite Hill", 
@@ -124,6 +136,28 @@ app.post("/campgrounds/:id/comments", function(req, res){
     //create new comment
     //connect new comment to campground
     //redirect campground show page
+});
+
+// ==============
+// AUTH ROUTES
+// ==============
+
+//show register form
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+//handle sign up logic
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register")
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds");
+        });
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
