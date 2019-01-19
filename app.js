58bf4566a091087986ba37a0dfb374478b75b6af
Linda Allen <lindaallenx@gmail.com>
2019-01-19 00:12:18 +0000

    logout/navbar
    * add logout route
    * prevent user from adding a comment if not signed in
    * add links to navbar
    * show/hide auth links

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ app.get("/campgrounds/:id", function(req, res){
 // COMMENTS ROUTES
 // =======================
 
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
     // find campground by id
     Campground.findById(req.params.id, function(err, campground){
         if(err){
@@ -115,7 +115,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
     })
 });
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
     //lookup campground using ID
     Campground.findById(req.params.id, function(err, campground){
        if(err){
@@ -171,6 +171,19 @@ app.post("/login", passport.authenticate("local",
         failureRedirect: "/login"
     }), function(req, res) {
 });
+
+//logout route
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/campgrounds");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The YelpCamp Server Has Started");
