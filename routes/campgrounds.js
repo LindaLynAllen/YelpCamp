ffef155e8f134b8c952e142b5d8d8c7812daf31f
Linda Allen <lindaallenx@gmail.com>
2019-01-20 21:34:13 +0000

    delete campgrounds
    * add destroy route
    * add delete button

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -68,6 +68,7 @@ router.get("/:id/edit", function(req, res) {
     });
 });
 
+//UPDATE CAMPGROUND ROUTE
 router.put("/:id", function(req, res){
     //find and update the correct campground
     Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
@@ -80,7 +81,16 @@ router.put("/:id", function(req, res){
     //redirect somewhere
 });
 
-//UPDATE CAMPGROUND ROUTE
+// DESTROY CAMPGROUND ROUTE
+router.delete("/:id", function(req, res){
+    Campground.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/campgrounds");
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
+});
 
 //middleware
 function isLoggedIn(req, res, next){
