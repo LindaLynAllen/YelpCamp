f29c2c6dbb50e971f1985ead98ce8637924d4a0c
Linda Allen <lindaallenx@gmail.com>
2019-01-20 21:14:21 +0000

    edit campgrounds
    * add Method-Override
    * add edit route for campgrounds
    * add link to edit page
    * add update route
    * fix $set problem

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var passport = require("passport");
 var LocalStrategy = require("passport-local");
+var methodOverride = require("method-override");
 var Campground = require("./models/campground");
 var Comment = require("./models/comment");
 var User = require("./models/user");
@@ -15,10 +16,10 @@ var campgroundRoutes = require("./routes/campgrounds");
 var indexRoutes = require("./routes/index");
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 // seedDB(); //seed the database
 
 // PASSPORT CONFIGURATION
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -57,6 +57,31 @@ router.get("/:id", function(req, res){
     });
 });
 
+//EDIT CAMPGROUND ROUTE
+router.get("/:id/edit", function(req, res) {
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            res.redirect("/campgrounds")
+        } else {
+            res.render("campgrounds/edit", {campground: foundCampground});
+        }
+    });
+});
+
+router.put("/:id", function(req, res){
+    //find and update the correct campground
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+        if(err){
+            res.redirect("/campgrounds");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+    //redirect somewhere
+});
+
+//UPDATE CAMPGROUND ROUTE
+
 //middleware
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
