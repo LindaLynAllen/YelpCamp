3638df7b196812376f44800dbb7cfb6dd89cf6e4
Linda Allen <lindaallenx@gmail.com>
2019-01-31 14:06:09 +0000

    move database to mlab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes = require("./routes/comments");
 var campgroundRoutes = require("./routes/campgrounds");
 var indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+mongoose.connect("mongodb://linda:ye!pcamp@ds023425.mlab.com:23425/yelpcamplm", { useNewUrlParser: true });
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
